15ec815013bcdcd48d0a8c0539018e0b8d2e4782
Ketan Ghumatkar <ketan@avaamo.com>
2021-04-20 17:33:56 +0530

    give full width to input field

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,4 +1,4 @@
 const action_name = "action_hello_world";
 const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
 const sender_id = uuidv4();
-const rasa_auto_suggestion = "http://localhost:5001/api/v2/rasa/autocomplete";
+const rasa_auto_suggestion = "http://localhost:5001/api/v1/resources/books";
